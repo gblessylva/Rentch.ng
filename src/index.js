ff954023fdf6cb193e2d1baf57e0cc79aa6b5a73
Sylva <gblessylva@gmail.com>
2019-09-27 06:44:21 +0100

    More Skeleton added

--- src/index.js
+++ src/index.js
@@ -21,18 +21,120 @@ function App() {
           <li><Link> Sign up</Link></li>
       </ul>
       </nav>
-          <section className="hero-section">
-          </section>
+        <section className="hero-section">
+          <h2>Search and Find Living Apartments</h2>
+            <div className="filter-wrapper">
+              <select >
+                <option>Space Type</option>
+              </select>
+              <select >
+                <option>Location</option>
+              </select>
+              <select >
+                <option>Category</option>
+              </select>
+              <button >
+                Search
+              </button>
+            </div>
+        </section>
       </header>
         <section className="popular-locations">
+          <h3> Popular Locations</h3>
+          <hr />
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p>
+          <div className="four-row">
+            <div className="row-card">
+            </div>
+            <div className="row-card">
+            </div>
+            <div className="row-card">
+            </div>
+            <div className="row-card">
+            </div>
+          </div>
         </section>
         <section className="featured-listing">
+          <h3> Featured Exclusives</h3>
+          <hr />
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p>
+          <div className="three-row">
+            <div className="row-card">
+            </div>
+            <div className="row-card">
+            </div>
+            <div className="row-card">
+            </div>
+          </div>
         </section>
         <section className="featured-people">
+          <h3> Featured People</h3>
+          <hr />
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p>
+          <div className="three-row">
+            <div className="people-card">
+            </div>
+            <div className="people-card">
+            </div>
+            <div className="people-card">
+            </div>
+            </div>
         </section>
         <section className="how-it--works">
+          <h3> How it Works</h3>
+          <hr />
+          <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p>
+          <div className="works-row">
+            <div className="work-card">
+              <div className="work-icon">
+              </div>
+              <h4> Search for a Space</h4>
+             <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+            </div>
+            <div className="work-card">
+                <div className="work-icon">
+                </div>
+                <h4> Book a Tour</h4>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+            </div>
+            <div className="work-card"> 
+              <div className="work-icon">
+              </div>
+              <h4> Move in</h4>
+              <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit,</p>
+            </div>
+          </div>
         </section>
         <section className="testimonials">
+            <h3> Testimonial</h3>
+            <hr />
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim</p>
+            <div className="testimonial-row">
+              <div className="testimonial-card">
+                <p>"</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut eni</p>
+                <hr />
+                <div className="testi-image">
+                </div>
+                <h4> Tony Stark</h4>
+              </div>
+              <div className="testimonial-card">
+                <p>"</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut eni</p>
+                <hr />
+                <div className="testi-image">
+                </div>
+                <h4> Tony Stark</h4>
+              </div>
+              <div className="testimonial-card">
+                <p>"</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut eni</p>
+                <hr />
+                <div className="testi-image">
+                </div>
+                <h4> Tony Stark</h4>
+              </div>
+          </div>
         </section>
         <section className="blog">
         </section>
