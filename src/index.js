e85bf4a93f024f1fdad022caefdcbe4c64a2acdf
Sylva <gblessylva@gmail.com>
2019-09-27 06:15:36 +0100

    Skeletons addes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./scss/index.scss"
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom"
 
-import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Router>
+    <main className="App">
+    <header >
+      <nav>
+        <div className="logo">
+           <h1>Rentch</h1>
+        </div>
+        <ul className="">
+          <li><Link> List a Space</Link></li>
+          <li><Link> Properties</Link></li>
+          <li><Link> Blog</Link></li>
+          <li><Link> Login</Link></li>
+          <li><Link> Sign up</Link></li>
+      </ul>
+      </nav>
+          <section className="hero-section">
+          </section>
+      </header>
+        <section className="popular-locations">
+        </section>
+        <section className="featured-listing">
+        </section>
+        <section className="featured-people">
+        </section>
+        <section className="how-it--works">
+        </section>
+        <section className="testimonials">
+        </section>
+        <section className="blog">
+        </section>
+        <section className="earn">
+        </section>
+        <footer >
+        </footer>
+    </main>
+    </Router>
   );
 }
 
